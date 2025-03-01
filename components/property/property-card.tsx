16857f14d8e9a437ac54e9c804a7154692182894
Marcos  Frias Molina <marcofrias0828@gmail.com>
2025-03-01 17:18:42 +0100

    Refactor PropertyCard and HeroSection components; enhance styling and responsiveness

--- components/property/property-card.tsx
+++ components/property/property-card.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 
 interface PropertyCardProps {
   id: string;
@@ -10,9 +11,11 @@ interface PropertyCardProps {
   bedrooms: number;
   bathrooms: number;
   imageUrl: string;
+  className?: string;
 }
 
 export function PropertyCard({
+  className,
   imageUrl,
   title,
   location,
@@ -22,12 +25,18 @@ export function PropertyCard({
   sqm,
 }: PropertyCardProps) {
   return (
-    <div className="property-card border-0 overflow-hidden">
+    <div
+      className={cn(
+        "relative rounded-xl border-0 overflow-hidden",
+        "max-lg:max-w-96 max-w-screen-sm mb-4 aspect-square",
+        className
+      )}
+    >
       <Image
         src={imageUrl}
         alt={title}
-        height={400}
-        width={400}
+        height={600}
+        width={600}
         className="w-full h-full object-cover hover:scale-110 transition-all duration-500"
       />
       <div className="absolute bottom-0 left-0 w-full bg-gradient-to-b from-transparent from-10% to-black pointer-events-none">
--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -1,49 +1,16 @@
 "use client";
 
-import { useLangStore } from "@/utils/store/lang-store";
-import { HeroSectionTranslations } from "@/lib/translations";
 import { cn } from "@/lib/utils";
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
-import Image from "next/image";
+import { useTranslation } from "react-i18next";
+import Article from "./article";
 
 gsap.registerPlugin(ScrollTrigger);
 
-interface HeroSectionProps {
-  language: AvailableLanguages;
-}
-
-interface ArticleProps {
-  imageSrc: string;
-  title: string;
-  description: string;
-  imageRef: React.RefObject<HTMLImageElement>;
-}
-
-const Article = ({ imageSrc, title, description, imageRef }: ArticleProps) => {
-  return (
-    <article className="flex flex-row w-full hover:scale-105 rounded-xl transition-transform duration-500 overflow-hidden">
-      <Image
-        ref={imageRef}
-        src={imageSrc}
-        alt={title}
-        height={0}
-        width={0}
-        className="w-1/2 h-fit"
-      />
-      <div className="p-4">
-        <h1 className="text-lg font-bold mb-2">{title}</h1>
-        <p className="text-sm text-muted-foreground">{description}</p>
-      </div>
-    </article>
-  );
-};
-
-export function HeroSection({ language }: HeroSectionProps) {
-  const { title, subtitle, cta } = HeroSectionTranslations[language];
-
+export function HeroSection() {
   const titleRef = useRef<HTMLHeadingElement>(null);
   const sectionRef = useRef<HTMLDivElement>(null);
   const imageRefs = [
@@ -58,16 +25,16 @@ export function HeroSection({ language }: HeroSectionProps) {
 
     gsap.fromTo(
       titleRef.current,
-      { y: 500, scale: 0.9, opacity: 0 },
+      { y: 200, scale: 0.4, opacity: 0 },
       {
-        y: 0,
+        y: -50,
         scale: 1,
         opacity: 1,
         duration: 1.5,
         scrollTrigger: {
           trigger: sectionRef.current,
           start: "top bottom",
-          end: "center center",
+          end: "20% 65%",
           scrub: true,
         },
       }
@@ -75,7 +42,7 @@ export function HeroSection({ language }: HeroSectionProps) {
 
     imageRefs.forEach((imageRef, index) => {
       gsap.from(imageRef.current, {
-        x: index % 2 === 0 ? -200 : 200,
+        x: index % 2 === 1 ? -200 : 200,
         opacity: 0,
         duration: 1.2,
         scrollTrigger: {
@@ -96,12 +63,12 @@ export function HeroSection({ language }: HeroSectionProps) {
         "Una residenza unica con vista mozzafiato e comfort moderni.",
     },
     {
-      imageSrc: "/residencial/2.webp",
+      imageSrc: "/residencial/3.webp",
       title: "Eleganza e Stile",
       description: "Appartamenti progettati per garantire il massimo comfort.",
     },
     {
-      imageSrc: "/residencial/3.webp",
+      imageSrc: "/residencial/2.webp",
       title: "Design Moderno",
       description: "Interni spaziosi e rifiniture di alta qualità.",
     },
@@ -116,27 +83,34 @@ export function HeroSection({ language }: HeroSectionProps) {
     <section
       ref={sectionRef}
       className={cn(
-        "relative w-full min-h-screen",
-        "bg-secondarybackground p-20"
+        "w-full h-[275vh] max-xl:h-[225vh] min-h-fit",
+        "bg-secondarybackground max-md:p-6 md:p-14 max-md:mt-20 p-20"
       )}
     >
       <h2
         ref={titleRef}
         className={cn(
-          "sticky top-20 z-20 text-center py-5",
-          "text-9xl font-serif uppercase bg-clip-text text-transparent bg-aurora"
+          "text-center",
+          "text-5xl md:text-7xl font-serif uppercase bg-clip-text text-transparent bg-aurora"
         )}
       >
-        {title}
+        Marbry Inmobiliaria
       </h2>
-
-      <div className="grid grid-rows-4 gap-6 mt-10">
+      <div className="relative bg-secondary-foreground max-w-screen-xl mx-auto w-full h-full rounded-xl">
         {articlesData.map((article, index) => (
           <Article
             key={index}
+            index={index}
             imageSrc={article.imageSrc}
             title={article.title}
             description={article.description}
+            className={cn(
+              index === 3 && "top-[3%] -left-72 lg:-left-32",
+              index === 2 && "top-[27%] -right-72 lg:-right-32",
+              index === 1 && "bottom-[27%] -left-72 lg:-left-32",
+              index === 0 && "bottom-[3%] -right-72 lg:-right-32"
+            )}
+            imageClassName="rounded-xl w-[600px] md:w-[900px] lg:w-[1100px] h-full aspect-video"
             imageRef={imageRefs[index] as React.RefObject<HTMLImageElement>}
           />
         ))}
